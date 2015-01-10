654cb9c7975b1725aa769aa73c98515049910c0d
younger.shen <younger.x.shen@gmail.com>
2015-01-10 15:34:05 +0800

    MIT LICENSE

--- setup.py
+++ setup.py
@@ -6,9 +6,21 @@ version = '0.0.1'
 setup(name='playwork',
       version=version,
       description="tornado, web framework, framework",
+
       long_description="""\
-a web framework based on the Tornado project""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      a web framework based on the Tornado project
+      """,
+
+      classifiers=[
+            "Development Status :: 1 - Planning",
+            "Environment :: Web Environment",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: MIT License",
+            "Natural Language :: English",
+            "Operating System :: POSIX",
+            "Programming Language :: Python :: 2.7",
+            "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+      ], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='playwork, tornado, web, webframework, framework',
       author='younger shen',
       author_email='younger.x.shen@gmail.com',
@@ -19,8 +31,11 @@ a web framework based on the Tornado project""",
       zip_safe=True,
       install_requires=[
           # -*- Extra requirements: -*-
+            "tornado>=4.0.2"
       ],
       entry_points="""
       # -*- Entry points: -*-
       """,
+      test_suite="playwork.test",
+      tests_require=["pyflakes>=0.6.1", "pep8>=1.4.1"],
       )
